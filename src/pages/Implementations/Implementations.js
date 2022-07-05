62c970cf8578545bc89d1f51f75fbd5ffda98feb
Ty Eggen <tycho.eggen@gmail.com>
2022-07-05 06:52:36 +0000

    Implementations: improved links

--- src/pages/Implementations/Implementations.js
+++ src/pages/Implementations/Implementations.js
@@ -6,9 +6,9 @@ const Implementations = () => (
     <h2>Implementations</h2>
     <p>The following implementations are under development:</p>
     <ul>
-      <li><a href="https://github.com/Colore/Colore-PHP">Colore-PHP</a> (mature)</li>
-      <li><a href="https://github.com/Colore/Colore-JS">Colore-JS</a> (design phase)</li>
-      <li><a href="https://github.com/Colore/Colore-J2EE">Colore-J2EE</a> (refactor in fork)</li>
+      <li><a href="https://github.com/Colore/Colore-PHP" target="_blank" rel="noreferrer">Colore-PHP</a> (mature)</li>
+      <li><a href="https://github.com/Colore/Colore-JS" target="_blank" rel="noreferrer">Colore-JS</a> (design phase)</li>
+      <li><a href="https://github.com/Colore/Colore-J2EE" target="_blank" rel="noreferrer">Colore-J2EE</a> (refactor in fork)</li>
     </ul>
   </div>
 )
